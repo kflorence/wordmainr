f7c9192d0afee6384a84600530571c95fdd02d0f
Kyle Florence <kyle.florence@gmail.com>
2010-12-15 22:17:10 -0800

    tabs to spaces

--- query.php
+++ query.php
@@ -5,22 +5,22 @@ $api_key = "your_wordnik_api_key";
 $wordmainr = new WordMainr($api_key, false);
 
 switch(isset($_GET["method"]) ? $_GET["method"] : "") {
-	case "getWords": {
-		echo $wordmainr->getWords(isset($_GET["query"]) ? $_GET["query"] : "", array("limit" => 100));
-		break;
-	}
-	
-	case "getDefinitions": {
-		echo $wordmainr->getDefinitions(isset($_GET["word"]) ? $_GET["word"] : "");
-		break;
-	}
-	
-	case "getDomains": {
-		echo $wordmainr->getDomains(isset($_GET["word"]) ? $_GET["word"] : "");
-		break;
-	}
-	
-	default: {
-		echo json_encode(array("error" => "Unknown request."));
-	}
+  case "getWords": {
+    echo $wordmainr->getWords(isset($_GET["query"]) ? $_GET["query"] : "", array("limit" => 100));
+    break;
+  }
+  
+  case "getDefinitions": {
+    echo $wordmainr->getDefinitions(isset($_GET["word"]) ? $_GET["word"] : "");
+    break;
+  }
+  
+  case "getDomains": {
+    echo $wordmainr->getDomains(isset($_GET["word"]) ? $_GET["word"] : "");
+    break;
+  }
+  
+  default: {
+    echo json_encode(array("error" => "Unknown request."));
+  }
 }
